4dc13a93682b95dd6a7208aadad15cb9618d0b5c
Maruthi Korrapati <kmpkumar@gmail.com>
2018-09-24 19:21:38 -0500

    final

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
-from flask import Flask
+from flask import Flask,request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+            <body>
+                <!-- create your form here -->
+                <from method="POST">
+                    <label>Rotate by: 
+                    <input name = "rot" type = "text" value='0'/>
+                    </label>
+                    <br/><br/>
+                    <textarea name ="text" ></textarea>
+                    <br/>
+                    <input type ="submit" />
+                </form>
+            </body>
+    </html>
+
+"""
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+
+   
+    text = request.form['text']
+    rot = int(request.form['rot'])
+    return '<h1>' + rotate_string(text, rot) + '</h1>'
+ 
+
 
 app.run()
 
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }
-        </style>
-    </head>
-    <body>
-      <!-- create your form here -->
-    </body>
-</html>
